bd6170eac87c5aaee028c3fc25b5781a66fc41ef
Zareef Razzaque <zareefrazzaque@outlook.com>
2024-05-04 22:52:51 +0100

    continuing tomorrow

--- graph.py
+++ graph.py
@@ -1,2 +1,21 @@
+import pyqtgraph as pg
 
-import matplotlib
+class Graph:
+    def __init__(self) -> None:
+        
+        self.y_axis = []
+        self.y_axis_title = 'y label not added'
+        
+        self.x_axis = []
+        self.x_axis_title = 'x label not added'
+        
+        self.graph_title = 'title not added'
+        
+        self.graph = pg.PlotWidget()
+        
+        
+    
+if __name__ == '__main__':
+    test = pg.QtGui.QApplication([])
+    test_widget = pg.plot()
+    
---
+++ stock.py
@@ -0,0 +1,11 @@
+class stock():
+    def __init__(self) -> None:
+        self.stock_name:str = None
+        self.currentValue:float = None
+        
+        self.graph1 = None
+        self.graph2 = None
+        self.graph3 = None
+        self.graph4 = None
+        
+    
